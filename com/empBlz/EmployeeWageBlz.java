e8bdd731b590b3355928a0cca1ea221a54ea4f53
Your Name <deepakbankar11111@gmail.com>
2022-09-11 10:47:50 -0700

    Added:Fulldayemployee Wage

--- com/empBlz/EmployeeWageBlz.java
+++ com/empBlz/EmployeeWageBlz.java
@@ -6,15 +6,25 @@ public class EmployeeWageBlz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int WagePerHr = 20;
+		int FullDayHr = 8;
 		System.err.println("Welcome To Employee Wage");
-		Random r=new Random();
-		int emp=r.nextInt(2);
-		if(emp==1)
-			System.out.println("Employee is Present");
-		else if(emp==0)
-			System.out.println("Employee is Absent");
-		else
-			System.out.println("Employee is not Working in company");
+		Random r = new Random();
+		int emp = r.nextInt(1);
+		int Salaryemp;
+		if (emp == 0) {
+
+			Salaryemp = FullDayHr * WagePerHr;
+
+			System.err.println("Salary Of full Day Employee is:  " + Salaryemp);
+		} else if(emp==1) {
+
+			System.err.println("Employee is half day");
+		}
+		else {
+			System.out.println("Employee is not Prsent");
+		}
 
 	}
 
