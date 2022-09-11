a14374446cba57d725303e97c2d307ed5527bd2c
Your Name <deepakbankar11111@gmail.com>
2022-09-11 10:22:25 -0700

    Added:Employee is prsent or absent

--- com/empBlz/EmployeeWageBlz.java
+++ com/empBlz/EmployeeWageBlz.java
@@ -1,10 +1,20 @@
 package com.empBlz;
 
+import java.util.Random;
+
 public class EmployeeWageBlz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.err.println("Welcome To Employee Wage");
+		Random r=new Random();
+		int emp=r.nextInt(2);
+		if(emp==1)
+			System.out.println("Employee is Present");
+		else if(emp==0)
+			System.out.println("Employee is Absent");
+		else
+			System.out.println("Employee is not Working in company");
 
 	}
 
